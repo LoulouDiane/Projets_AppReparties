e6b1995899e1f0f1d6bd22cdaeb2ad576910edec
Naomie Kabila <59931823+LoulouDiane@users.noreply.github.com>
2022-07-10 08:11:28 +0200

    Add files via upload

---
+++ Sockets/Exemple2/Client.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+import socket
+s=socket.socket()
+port = 5000
+s.connect(('127.0.0.1', port))
+input_string=input("Entrer les donnees a envoyer >")
+s.sendall(input_string.encode())
+print (s.recv(1024))
+s.close ()
