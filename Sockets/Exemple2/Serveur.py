f4b1c1709f9da62a03d492ae7b5e450b877cdca9
Naomie Kabila <59931823+LoulouDiane@users.noreply.github.com>
2022-07-10 08:58:13 +0200

    Update Serveur.py

--- Sockets/Exemple2/Serveur.py
+++ Sockets/Exemple2/Serveur.py
@@ -8,12 +8,13 @@ s.bind(('0.0.0.0',port))
 print("socket attache a %s" %(port))
 s.listen(5)
 print("socket en ecoute !!!")
+
 while True:
     c, addr = s.accept()
     print('recpetion des donnees provenant de ', addr)
     data = c.recv(1024)
     
-
+    
     for i in range (0, len(data)):
         if(data[i]=='a' or data [i]=='e' or data[i]=='i' or data[i]=='o' or data[i]=='u' or data[i]=='A' or data[i]=='E' or data[i]=='I' or data[i]=='o'):
             data= data.replace(i, '#')
